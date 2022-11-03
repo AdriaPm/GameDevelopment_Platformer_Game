9ce2852d9e7defa372eb67a731e0808dd774e68b
Adrià Pons Mensa <adriapm1950@gmail.com>
2022-11-03 11:10:59 +0100

    CameraFix collider

--- citm_desvj_project_template-L07/Game/Source/Map.cpp
+++ citm_desvj_project_template-L07/Game/Source/Map.cpp
@@ -238,7 +238,10 @@ bool Map::Load()
 
      // WATER collider
      PhysBody* c28 = app->physics->CreateRectangle(0+1680, 560+40, 32*105, (32*2)+16, STATIC, ColliderType::WATER);
+
+     PhysBody* c29 = app->physics->CreateRectangle((32*16)+27 ,(32*13)+64 , 10, 32*4, STATIC, ColliderType::CAMERAFIX);
     
+
 
 
     if(ret == true)
